867c4cd8b797bcf42c729ccaf1d876b14fa7bf30
anuragsingh45 <anuragsingh.outlook@gmail.com>
2025-09-16 11:07:23 +0530

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,8 +10,8 @@ export default function DashboardLayout({
     <>
             {children}
 <footer className="w-full bg-zinc-300 shadow-lg text-gray-900 py-4">
-      <div className="max-w-7xl px-4 flex flex-col items-center sm:flex-row sm:justify-center text-sm text-center sm:text-left">
-        <p className="mb-2 text-center sm:mb-0">
+      <div className="max-w-7xl px-4 flex flex-col items-center sm:flex-row sm:justify-edit text-sm text-center sm:text-left">
+        <p className="mb-2 px-2 text-center sm:mb-0">
           © 2025 <span className="font-semibold text-black">Digitalbuzz</span>. All rights reserved.
         </p>
         <p>
